ab0364f0bb0943ddfea5d669f50975e15bed905f
HashT9u <61894183+HashT9u@users.noreply.github.com>
2021-04-06 15:02:39 -0400

    Update example.js

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 const topgg = require('better-topgg-api')
 
-const Client = new topgg.Client("YOUR_TOPGG_WEBHOOK_TOKEN")
+const Client = new topgg.Client("YOUR_TOPGG_WEBHOOK_TOKEN", client)
 
 Client.getClientInfo({clientID: '803362044048572456'}).then(console.log)
